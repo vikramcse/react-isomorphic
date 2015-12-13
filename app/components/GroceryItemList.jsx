bb6a770b99381b7de05f3689b30be528ecbf7b7c
vikramcse <vikramcse.10@gmail.com>
2015-12-13 23:25:16 +0530

    Changed for the form item

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -1,5 +1,6 @@
 var React =	require('react'); // here comes useful browserify
 var GroceryItem = require('./GroceryItem.jsx');
+var GroceryListAddItem = require('./GroceryListAddItem.jsx');
 
 module.exports = React.createClass({
     render: function() {
@@ -15,6 +16,7 @@ module.exports = React.createClass({
                         })
                     }
                 </div>
+                <GroceryListAddItem />
             </div>
         );
     }
